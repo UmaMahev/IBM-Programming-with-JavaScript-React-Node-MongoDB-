1f14e25d70c9349e5e1740196aac62d866ce4a34
UmaMahev <m.umamaheswari@outlook.com>
2026-02-18 14:12:28 -0500

    Product Survey

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,39 @@
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick=submitFeedback;
+function submitFeedback(){
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const userExperience=document.getElementById('userExperince').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    
+    console.log('Feedback submitted:', {
+        username,
+        age,
+        email,
+        job,
+        designation,
+        productType,
+        feedback
+    });
+
+
+    document.getElementById('userName').innerHTML = username;
+    document.getElementById('userAge').innerHTML = age;
+    document.getElementById('userEmail').innerHTML = email;
+    document.getElementById('userJob').innerHTML = job;
+    document.getElementById('userDesignation').innerHTML = designation;
+    document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML=userExperience;
+    document.getElementById('userInfo').style.display = 'block';
+    
+}
+
+document.addEventListener('keydown',function(event){
+if(event.key==='Enter'){
+    submitFeedback();
+}   });
